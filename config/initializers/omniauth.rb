a7fcc14fdd96eaf8df0ab4032951ffe194ec1d2d
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-11-07 01:03:20 -0200

    Login with Facebook #2

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '191320974537872', '65ebfa58974e0f401f7ddbfa6a1c7d3d'
+end
