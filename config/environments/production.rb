477b25f0857969bfe1e76bdec9bf9f8c8fde41e3
Alessandro Caetano <alessandro.caetanob@gmail.com>
2015-11-10 01:29:57 -0200

    Adding base configuration for materilize
    Signed-off-by: Alessandro Caetano <alessandro.caetanob@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  #Used to load static assets 
+  config.serve_static_files = false
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
