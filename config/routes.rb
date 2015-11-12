4e2a54dced16ce069d45ce8e04686d02fc273589
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-11-12 00:12:56 -0200

    See btn
    Signed-off-by: Rodrigo Gonçalves <rodrigosg2000@gmail.com>
    Signed-off-by: Alessandro Caetano <alessandro.caetanob@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
       get 'inscribe'
     end
   end
-  root :to => 'home#index'
-  get '/:id', :to => 'home#show', :as => 'user_page'
+  root to: 'home#index'
+  get '/:id', to: 'home#show', as: 'user_page'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
