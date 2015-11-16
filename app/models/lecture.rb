e6f35c8242746af8fa2b891be47d4b175da26d51
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-11-16 12:57:14 -0200

    Fixing gambira

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,4 +2,8 @@ class Lecture < ActiveRecord::Base
   has_many :enrollments
   has_many :users, through: :enrollments
   belongs_to :user, foreign_key: 'speaker_id'
+
+  def speaker
+    User.find(self.speaker_id)
+  end
 end
