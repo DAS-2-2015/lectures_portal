ee6ad532760d10c0df12fd2bcc0ab4d7085a64cb
yeltsinlobato <yeltsin.lobato@gmail.com>
2015-11-22 10:54:09 -0200

    Make adjustments in Lecturers review

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'lectures', to: 'lectures#index'
+  get 'lecturers', to: 'users#index'
 
   # resources :users do
   resources :reviews
