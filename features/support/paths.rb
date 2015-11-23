b17e3cf8d3acfebd924dbdd29b09fe7c671ad64d
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-11-23 15:40:55 -0200

    Edit lecture feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,6 +12,8 @@ module NavigationHelpers
       '/'
     when /^home page$/
       '/home'
+    when /^lectures page$/
+      '/lectures'
       
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
