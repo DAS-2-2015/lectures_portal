330aa38e628e95b87cfeb725383cd7f5783ce3ef
yeltsinlobato <yeltsin.lobato@gmail.com>
2015-11-23 10:33:16 -0200

    Create lecturers recomendations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,32 @@ class HomeController < ApplicationController
 
   def show
   	@user = User.find_by_id session[:user_id]
+  	
+    if @user.lectures.count == 0
+      ranking_lecturers
+    else
+      self.recomendation_lecturers
+    end
   end
+
+  def ranking_lecturers
+    @users = User.order(:rate).limit(5).where.not(id: @user.id).reverse
+  end
+
+  def recomendation_lecturers
+    lecturers_ranked = []
+
+    @user.lectures(limit: 5).reverse.each do |user_lecture| 
+      Lecture.find_each do |lecture|
+        if lecture.title == user_lecture.title
+          if lecture.user != @user
+            lecturers_ranked.push(lecture.user)
+          end
+        end
+      end
+    end
+    lecturers_ranked = lecturers_ranked.uniq
+    @users = lecturers_ranked.sort_by {|obj| obj.rate}.reverse
+  end
+    
 end
